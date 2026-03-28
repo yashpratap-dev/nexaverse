e65c6c73de5e169f75edd09672384644dad60d24
Yash <prabhjotsingh5424@email.com>
2026-03-28 17:14:31 +0530

    feat: Week 6 - Dockerfile + deployment config

--- src/test/java/com/nexaverse/nexaverse/NexaverseApplicationTests.java
+++ src/test/java/com/nexaverse/nexaverse/NexaverseApplicationTests.java
@@ -2,12 +2,13 @@ package com.nexaverse.nexaverse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class NexaverseApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
