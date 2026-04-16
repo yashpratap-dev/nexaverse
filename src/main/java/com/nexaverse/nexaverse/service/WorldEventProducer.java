96a147e1fce9ebe68e28a03e40138b01af5d89c7
yashpratap-dev <prabhjotsingh5424@gmail.com>
2026-04-17 02:55:16 +0530

    fix: WorldEventProducer always a bean, Kafka optional

--- src/main/java/com/nexaverse/nexaverse/service/WorldEventProducer.java
+++ src/main/java/com/nexaverse/nexaverse/service/WorldEventProducer.java
@@ -2,13 +2,11 @@ package com.nexaverse.nexaverse.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-@ConditionalOnBean(KafkaTemplate.class)
 public class WorldEventProducer {
 
     @Autowired(required = false)
